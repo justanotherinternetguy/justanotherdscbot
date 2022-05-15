bf89ed3701bf049e4bd1598095e009773064fbd4
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-05-15 02:03:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,27 @@
 
 import random
 import discord
+import humanfriendly
 import os
+import re
 from dotenv import load_dotenv
 import requests
 import json
 import asyncio
 import time
-from discord.ext import commands
+from discord.ext import commands, tasks
+import functools
+import itertools
+import youtube_dl
+from discord.utils import get
+from keep_alive import keep_alive
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-intents = discord.Intents.default()
-intents.members = True
+intents = discord.Intents().all()
 description="justanotherdscbot: MADE BY justanotherinternetguy#6982"
-
+client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='~', intents=intents) 
 
 def get_quote():
@@ -36,6 +42,7 @@ async def on_ready():
     print("Logged in as:")
     print(bot.user.name)
     print(bot.user.id)
+    print(discord.__version__)
     print("_____________")
 
 @bot.event
@@ -58,7 +65,7 @@ async def on_message_delete(message):
 @bot.command()
 async def ping(ctx):
     """Pong!"""
-    await ctx.send('Hello {}'.format(ctx.author.mention))
+    await ctx.reply('Hello {}'.format(ctx.author.mention))
 
 @bot.command()
 async def roll(ctx, dice: str):
@@ -66,11 +73,11 @@ async def roll(ctx, dice: str):
     try:
         rolls, limit = map(int, dice.split('+'))
     except Exception:
-        await ctx.send('{} - Format must be in `<# of dice>+<# of faces per die>`'.format(ctx.author.mention))
+        await ctx.reply('{} - Format must be in `<# of dice>+<# of faces per die>`'.format(ctx.author.mention))
         return
 
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
+    await ctx.reply(result)
 
 
 @bot.command()
@@ -79,7 +86,7 @@ async def joininfo(ctx, member: discord.Member):
     await ctx.send('{0.name}, `{0.id}` joined in {0.joined_at}'.format(member))
 
 @bot.command()
-async def getquote(ctx):
+async def inspire(ctx):
     """Get an inspirational quote"""
     quote = get_quote()
     await ctx.reply(quote, mention_author=True)
@@ -92,14 +99,17 @@ async def howlarge(ctx):
 
 
 @bot.command()
-@commands.has_permissions(ban_members=True)
+@commands.has_permissions(administrator=True)
 async def ban(ctx, member : discord.Member, reason=None):
     """Bans a user"""
+    if member.id == ctx.author.id:
+        await ctx.reply("You can't ban yourself!")
+        return;
     if reason == None:
-        await ctx.send(f"Woah {ctx.author.mention}, Make sure you provide a reason!")
+        await ctx.reply(f"Woah {ctx.author.mention}, Make sure you provide a reason!")
     else:
         messageok = f"You have been **banned** from {ctx.guild.name} for {reason}"
-        to_send = '{0.mention} has been **banned**!'.format(member)
+        to_send = '{0.mention}, {1} has been **banned**!'.format(member, member.id)
         await ctx.reply(to_send)
         await member.send(messageok)
         await member.ban(reason=reason)
@@ -117,13 +127,16 @@ async def unban(ctx, *, member):
 
         if (user.name, user.discriminator) == (member_name, member_discriminator):
             await ctx.guild.unban(user)
-            await ctx.send(f'Unbanned {user.mention}')
+            await ctx.reply(f'Unbanned {user.mention}')
 
 
 @bot.command()
-@commands.has_permissions(ban_members=True)
+@commands.has_permissions(administrator=True)
 async def kick(ctx, member : discord.Member, reason=None):
     """Kicks a user"""
+    if member.id == ctx.author.id:
+        await ctx.reply("You can't kick yourself!")
+        return;
     if reason == None:
         await ctx.send(f"Woah {ctx.author.mention}, Make sure you provide a reason!")
     else:
@@ -134,4 +147,54 @@ async def kick(ctx, member : discord.Member, reason=None):
         await member.kick(reason=reason)
 
 
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def create_role(ctx, name, r, g, b, permissions):
+	"""Creates a custom role
+
+	Creates a custom roles with RGB colors and custom permissions
+
+	USE https://discordapi.com/permissions.html to calculate permission value
+	
+	"""
+	guild = ctx.guild
+	await guild.create_role(name=name, color=discord.Color.from_rgb(int(r), int(g), int(b)), permissions=discord.Permissions(permissions=int(permissions)))
+	await ctx.reply("Role {} created!".format(name))
+
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def add_role(ctx, name, member : discord.Member):
+	"""Assign a premade role to a user"""
+	role = discord.utils.get(ctx.guild.roles, name=name)
+	await member.add_roles(role)
+	await ctx.reply("Role {} added to {}".format(name, member))
+
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def rm_role(ctx, name, member : discord.Member):
+	"""Remove a role from a user"""
+	role = discord.utils.get(ctx.guild.roles, name=name)
+	await member.remove_roles(role)
+	await ctx.reply("Role {} removed from {}".format(name, member))
+
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def delete_role(ctx, name):
+	"""Delete a role from the server"""
+	role = discord.utils.get(ctx.message.guild.roles, name=name)
+	await role.delete()
+	await ctx.reply ("Role {} deleted!".format(name))
+
+# await ctx.channel.purge(limit = 1)
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def purge(ctx, count):
+	"""Purge messages from chat"""
+	await ctx.channel.purge(limit = int(count))
+
+	
+keep_alive()
 bot.run(DISCORD_TOKEN)
